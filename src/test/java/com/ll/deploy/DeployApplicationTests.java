c5778791b89caf772ffbd65c59eb3111c2def041
leedahye426 <9535957@naver.com>
2023-12-22 21:42:00 +0900

    prod, test

--- src/test/java/com/ll/deploy/DeployApplicationTests.java
+++ src/test/java/com/ll/deploy/DeployApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ll.deploy;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DeployApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
